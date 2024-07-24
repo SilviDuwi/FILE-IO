5866772fd706af33d8833a2d88b3f21235c2dad0
duwinabila861@gmail.com <duwinabila861@gmail.com>
2024-07-24 12:00:41 +0700

    latihanfileio

---
+++ baca_file2.py
@@ -0,0 +1,13 @@
+# buka file
+file_pantun = open("pantun.txt", "r")
+
+# baca isi file
+print (file_pantun.read())
+
+# cetak baris pertama 
+
+# cetak baris kedua
+#print (pantun[1])
+
+# tutup file
+file_pantun.close()
